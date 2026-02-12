f4b5690a8031993c9e0b22372473bbefe7b0c211
chim borey <oceansdtol@gmail.com>
2026-02-12 22:25:52 +0700

    add

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -4,6 +4,7 @@ import { FaTrash } from "react-icons/fa";
 import { IoRemove } from 'react-icons/io5';
 import { RiShoppingBag3Line } from "react-icons/ri";
 import { IoMdAdd } from 'react-icons/io';
+import "react-icons/fa"
 const Cart = () => {
   const { cart, removeFromCart, clearCart, increaseQuantity, decreaseQuantity, quantity, total } = useContext(ShopContext)
   return (
